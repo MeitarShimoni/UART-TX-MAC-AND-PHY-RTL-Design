2ef8a9b05fc27945804d4747c18c33a55c1520cc
Meitar Shimoni <141142200+MeitarShimoni@users.noreply.github.com>
2025-12-03 20:48:19 +0200

    Update clock_divider32.sv

--- design/clock_divider32.sv
+++ design/clock_divider32.sv
@@ -1,17 +1,16 @@
+// FILE : CLOCK DIVIDER
+// Author : MEITAR SHIMONI
+// DESCRIPTION : Creates Clock enable for FPGA Methodology at Baud Rate of 57600bps.
 module clock_divider32#(parameter int div_by = 31250)(
     input sys_clk,
     input reset_n,
-    //input CE,
     output reg clk_32
 );
 
-// 2^15 is enoght
-reg [15:0] count;
-
+reg [15:0] count; // update to $clog
 
 always @(posedge sys_clk or negedge reset_n) begin
     if(!reset_n) begin
-        // reset
         count <= 0;
         clk_32 <= 0;
     end else begin
@@ -25,22 +24,5 @@ always @(posedge sys_clk or negedge reset_n) begin
     end
 
 end
-
-// add to input CE.
-//always_ff @(posedge sys_clk or negedge reset_n) begin
-//    if(!reset_n) begin
-//      clk_32 <= 0;
-//      count <= 0;
-//    end else if(CE) begin
-//        if(count == 12) begin
-//            clk_32 <= 1;
-//            count <= 0;
-//        end else begin
-//            count++;
-//            clk_32 <= 0;
-//        end 
-//    end else clk_32 <= 0;
-//end
-
 
 endmodule
